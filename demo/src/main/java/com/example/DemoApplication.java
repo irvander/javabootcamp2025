f4020e5c5b4afd97f91fccfcb10220b43aa8ba7f
Van Der Kleijn Méndez <iria-lua.van-der-kleijn-mendez@capgemini.com>
2025-03-13 16:18:54 +0100

    clean demo

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,64 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Sort;
+
+import com.example.domains.contracts.repositories.ActorsRepository;
+import com.example.domains.entities.Actor;
+
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.example.ioc")
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada");
+		exampleData();
+	}
+	
+	@Autowired
+	private ActorsRepository dao;
+	
+	private void exampleData() {
+		//var actor = new Actor(0, "Emilie", "Valey");
+		//dao.save(actor);
+/*		var item = dao.findById(201);
+		if(item.isPresent()) {
+			var actor = item.get();
+			actor.setFirstName("EMILIE");
+			actor.setLastName(actor.getLastName().toUpperCase());
+			dao.save(actor);
+		} else {
+			System.err.println("No se ha encontrado el actor.");
+		}
+		
+		dao.findAll().forEach(System.err::println);
+		dao.deleteById(201);
+		dao.findAll().forEach(System.err::println);
+	
+		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("e").forEach(System.err::println);
+		dao.findTop10ByFirstNameStartingWith("e", Sort.by("FirstName").ascending()).forEach(System.err::println);
+
+		dao.findByActorIdGreaterThan(198).forEach(System.err::println);
+		dao.findNovedadesJPQL(198).forEach(System.err::println);
+		dao.findNovedadesSQL(198).forEach(System.err::println);
+*/	
+		//dao.findAll((root, query, builder) -> builder.lessThanOrEqualTo(root.get("actorId"), 5), null).forEach(System.err::println);
+	
+		
+	}
+	
+}
