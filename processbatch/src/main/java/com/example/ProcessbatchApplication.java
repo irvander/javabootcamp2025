308693e5e553f17f51b85b903ce8d364e44c51d7
irvander <irvander@LES-GX3LKN3.corp.capgemini.com>
2025-03-18 16:13:25 +0100

    XML

--- processbatch/src/main/java/com/example/ProcessbatchApplication.java
+++ processbatch/src/main/java/com/example/ProcessbatchApplication.java
@@ -1,13 +1,27 @@
 package com.example;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProcessbatchApplication {
+public class ProcessbatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessbatchApplication.class, args);
 	}
 
+	@Autowired
+	JobLauncher jobLauncher;
+	
+	@Autowired
+	private Job personasJob;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
