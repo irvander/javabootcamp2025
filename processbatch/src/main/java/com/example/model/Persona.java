32f6515dd592e12a52ea24f42fd3a0ac3c9d256c
irvander <irvander@LES-GX3LKN3.corp.capgemini.com>
2025-03-18 12:55:53 +0100

    config

--- processbatch/src/main/java/com/example/model/Persona.java
+++ processbatch/src/main/java/com/example/model/Persona.java
@@ -3,5 +3,34 @@ package com.example.model;
 public class Persona {
 	private long id;
 	private String nombre, correo, ip;
-	// Ctor(), Get/Set, toString(), …
+	public long getId() {
+		return id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", correo=" + correo + ", ip=" + ip + "]";
+	}
+	
+	
 }
