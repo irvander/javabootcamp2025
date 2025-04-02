98f300ff50fce36f766a373796c66b385a97e3e0
irvander <iria-lua.van-der-kleijn-mendez@capgemini.com>
2025-04-02 09:10:49 +0200

    Add demos test

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
+import { NotificationComponent } from './MainModule/main';
+import { DemosComponent } from './demos/demos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotificationComponent, DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-ng';
-
-  // constructor(out: LoggerService) {
-  //   out.error('Error')
-  //   out.warn('Warn')
-  //   out.info('Info')
-  //   out.log('Log')
-  // }
 }
---
+++ demo-ng/src/app/demos/demos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NotificationService } from '../common-services';
+
+@Component({
+  selector: 'app-demos',
+  imports: [],
+  templateUrl: './demos.component.html',
+  styleUrl: './demos.component.css'
+})
+export class DemosComponent {
+  constructor(public vm: NotificationService) { }
+}
