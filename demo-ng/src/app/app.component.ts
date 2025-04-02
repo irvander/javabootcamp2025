9c0a0ef034e2e0d9caabf6a51f562e8109294a7b
irvander <iria-lua.van-der-kleijn-mendez@capgemini.com>
2025-04-02 09:30:48 +0200

    Add modal notif

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
-import { NotificationComponent } from './MainModule/main';
+import { NotificationComponent, NotificationModalComponent } from './main';
 import { DemosComponent } from './demos/demos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NotificationComponent, DemosComponent],
+  imports: [RouterOutlet, NotificationComponent, NotificationModalComponent, DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
