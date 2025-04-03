24bb5c2c05fd0930bba4d25af32f3c1ae6b9ffc8
irvander <iria-lua.van-der-kleijn-mendez@capgemini.com>
2025-04-03 11:46:30 +0200

    Delete function

--- moviesapp/src/main/java/com/example/application/resources/ActorsResource.java
+++ moviesapp/src/main/java/com/example/application/resources/ActorsResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,12 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.domains.contracts.services.ActorsService;
 import com.example.domains.entities.dtos.ActorDTO;
+import com.example.domains.entities.dtos.ActorShort;
 import com.example.exceptions.BadRequestException;
 import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
@@ -46,8 +49,11 @@ public class ActorsResource {
 
 	@GetMapping
 	@Hidden
-	public List<ActorDTO> getAll() {
-		return srv.getByProjection(ActorDTO.class);
+	public List<?> getAll(@RequestParam(required = false, defaultValue = "long") String format) {
+		if("short".equals(format))
+			return (List<?>) srv.getByProjection(Sort.by("firstName", "lastName"), ActorShort.class);
+		else
+			return (List<?>) srv.getByProjection(Sort.by("firstName", "lastName"), ActorDTO.class); // srv.getAll();;
 	}
 
 	@GetMapping(params = { "page" })
@@ -55,7 +61,7 @@ public class ActorsResource {
 	public Page<ActorDTO> getAll(@ParameterObject Pageable pageable) {
 		return srv.getByProjection(pageable, ActorDTO.class);
 	}
-
+	
 	@GetMapping(path = "/{id}")
 	@Operation(summary = "Obtiene un actor por su id")
 	public ActorDTO getOne(@PathVariable @Parameter(description = "Identificador del actor") int id) throws NotFoundException {
